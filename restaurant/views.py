d049260e011e35f4a7e9d198cce64e6ed1816117
Ahsan Farooq <ahsan.farooq@yahoo.com>
2023-03-01 18:24:28 -0800

    Add files via upload

---
+++ restaurant/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from rest_framework import generics, viewsets
+from . models import Booking, Menu
+from . serializers import BookingSerializer, MenuSerializer
+from rest_framework.permissions import IsAuthenticated
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    permissions = [IsAuthenticated]
+    
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    permissions = [IsAuthenticated]
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permissions = [IsAuthenticated]
